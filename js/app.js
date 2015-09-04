103832f3a0fc1d60ab32ff69810bad6fed4f67b1
Nguyen Vinh <ninjaprox@gmail.com>
2015-09-04 17:34:02 +0400

    Example

--- js/app.js
+++ js/app.js
@@ -1,5 +1,122 @@
 (function() {
     'use strict';
 
-    angular.module('app', []);
+    var app = angular.module('app', []);
+
+    app.controller('MainController', ['$scope', function($scope) {
+        $scope.fields = [{
+            fieldName: 'field1',
+            columnName: 'Field 1'
+        }, {
+            fieldName: 'field2',
+            columnName: 'Field 2'
+        }, {
+            fieldName: 'field3',
+            columnName: 'Field 3'
+        }, {
+            fieldName: 'field4',
+            columnName: 'Field 4'
+        }, {
+            fieldName: 'field5',
+            columnName: 'Field 5'
+        }];
+        $scope.fixedFields = [{
+            fieldName: 'field1',
+            columnName: 'Field 1'
+        }];
+        $scope.fluidFields = [{
+            fieldName: 'field2',
+            columnName: 'Field 2'
+        }, {
+            fieldName: 'field3',
+            columnName: 'Field 3'
+        }, {
+            fieldName: 'field4',
+            columnName: 'Field 4'
+        }, {
+            fieldName: 'field5',
+            columnName: 'Field 5'
+        }];
+        $scope.data = [{
+            id: 1,
+            field1: 'data',
+            field2: 'data',
+            field3: 'data',
+            field4: 'data',
+            field5: 'data'
+        }, {
+            id: 2,
+            field1: 'data',
+            field2: 'data',
+            field3: 'data',
+            field4: 'data',
+            field5: 'data'
+        }, {
+            id: 3,
+            field1: 'data',
+            field2: 'data',
+            field3: 'data',
+            field4: 'data',
+            field5: 'data'
+        }, {
+            id: 4,
+            field1: 'data',
+            field2: 'data',
+            field3: 'data',
+            field4: 'data',
+            field5: 'data'
+        }, {
+            id: 5,
+            field1: 'data',
+            field2: 'data',
+            field3: 'data',
+            field4: 'data',
+            field5: 'data'
+        }, {
+            id: 6,
+            field1: 'data',
+            field2: 'data',
+            field3: 'data',
+            field4: 'data',
+            field5: 'data'
+        }, {
+            id: 7,
+            field1: 'data',
+            field2: 'data',
+            field3: 'data',
+            field4: 'data',
+            field5: 'data'
+        }, {
+            id: 8,
+            field1: 'data',
+            field2: 'data',
+            field3: 'data',
+            field4: 'data',
+            field5: 'data'
+        }, {
+            id: 9,
+            field1: 'data',
+            field2: 'data',
+            field3: 'data',
+            field4: 'data',
+            field5: 'data'
+        }, {
+            id: 10,
+            field1: 'data',
+            field2: 'data',
+            field3: 'data',
+            field4: 'data',
+            field5: 'data'
+        }];
+
+        $scope.onDelete = function(selection) {
+            console.log('onDelete: ' + JSON.stringify(selection));
+        };
+        $scope.onAdd = function() {
+            console.log('onAdd');
+        };
+        $scope.onDetail = function(data) {
+            console.log('onDetail: ' + JSON.stringify(data));
+        };
+    }]);
 })();
