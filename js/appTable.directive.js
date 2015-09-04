82c3208b022ac3b0773ee501ba3ff8b65e0ee553
Nguyen Vinh <ninjaprox@gmail.com>
2015-09-04 15:55:29 +0400

    Generate table headers

--- js/appTable.directive.js
+++ js/appTable.directive.js
@@ -6,14 +6,44 @@
             return {
                 restrict: 'E',
                 templateUrl: 'template/appTable-fixed.html',
-                scope: {},
+                scope: {
+                    data: '=',
+                    type: '@',
+                    fields: '=?',
+                    fixedFields: '=?',
+                    fluidFieldx: '=?',
+                    fixedTableClass: '@',
+                    fluidTableClass: '@',
+                    customContent: '=?',
+                    onDetail: '@',
+                    onAdd: '@',
+                    onDelete: '@'
+                },
                 link: function($scope, $element, $attrs) {
 
                 },
                 controller: function($scope, $element, $attrs) {
                     var vm = this;
+
+                    // Properties
+                    vm.headerNames = [];
+
+                    init();
+
+                    function init() {
+                        // Extract headers
+                        vm.headers = extractHeaders(vm.fields);
+                    }
+
+                    // Privates
+                    function extractHeaders(fields) {
+                        return fields.map(function(field) {
+                            return field.columnName || field.fieldName;
+                        });
+                    }
                 },
-                controllerAs: 'appTableCtrl'
+                controllerAs: 'appTableCtrl',
+                bindToController: true
             }
         }]);
 })();
